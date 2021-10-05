8d73a4fe464e12832b9a39005afce73c7912d16c
sw24sx <aleksandr.korolyov.99@mail.ru>
2021-10-05 14:26:48 +0500

    Start

---
+++ src/main/java/com/example/telephony/TelephonyApplication.java
@@ -0,0 +1,13 @@
+package com.example.telephony;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelephonyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelephonyApplication.class, args);
+    }
+
+}
