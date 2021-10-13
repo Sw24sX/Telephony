d957a85be85900917127195c85317480d321eaed
sw24sx <aleksandr.korolyov.99@mail.ru>
2021-10-13 11:21:45 +0500

    Add crud for caller

--- src/main/java/com/example/telephony/mapper/CallerMapper.java
+++ src/main/java/com/example/telephony/mapper/CallerMapper.java
@@ -4,8 +4,12 @@ import com.example.telephony.domain.Caller;
 import com.example.telephony.dto.CallerDto;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface CallerMapper {
     CallerDto callerToCallerDto(Caller caller);
     Caller callerToCallerDto(CallerDto callerDto);
+    List<CallerDto> listCallerToCallerDto(List<Caller> callers);
+    List<Caller> listCallerDtoToCaller(List<CallerDto> callerDtos);
 }
