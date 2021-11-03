d0664f63281cb06bd7b5f05936858dae70d99151
sw24sx <aleksandr.korolyov.99@mail.ru>
2021-11-04 01:42:06 +0500

    add wrong result tests

--- src/test/java/com/example/telephony/ExelParsingFileTest.java
+++ src/test/java/com/example/telephony/ExelParsingFileTest.java
@@ -2,25 +2,20 @@ package com.example.telephony;
 
 import com.example.telephony.common.CallersBaseBuilder;
 import com.example.telephony.domain.CallersBase;
+import com.example.telephony.enums.FileParsingExceptionMessage;
 import com.example.telephony.exception.FileParsingException;
 import com.example.telephony.service.file.FileParseService;
-import com.google.common.collect.Lists;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.util.Assert;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
-import java.util.List;
 
 @SpringBootTest
 public class ExelParsingFileTest {
@@ -28,15 +23,18 @@ public class ExelParsingFileTest {
     private FileParseService fileParseService;
 
     private final static String RELATIVE_WAY = "src\\test\\java\\com\\example\\telephony\\data\\exel";
-    private final static String RELATIVE_WAY_CORRECT_FILES = "correct";
-    private final static String RELATIVE_WAY_WRONG_FILES = "wrong";
+    private final static String CORRECT_FILES = "correct";
+    private final static String WRONG_FILES = "wrong";
+    private final static String NOT_VALID_FILES = "not_valid";
 
-    private final Path absolutePathCorrectFiles;
-    private final Path absolutePathWrongFiles;
+    private final Path correctFiles;
+    private final Path wrongFiles;
+    private final Path notValidFiles;
 
     public ExelParsingFileTest() {
-        this.absolutePathCorrectFiles = Paths.get(RELATIVE_WAY).resolve(RELATIVE_WAY_CORRECT_FILES).toAbsolutePath();
-        this.absolutePathWrongFiles = Paths.get(RELATIVE_WAY).resolve(RELATIVE_WAY_WRONG_FILES).toAbsolutePath();
+        this.correctFiles = Paths.get(RELATIVE_WAY).resolve(CORRECT_FILES).toAbsolutePath();
+        this.wrongFiles = Paths.get(RELATIVE_WAY).resolve(WRONG_FILES).toAbsolutePath();
+        this.notValidFiles = Paths.get(RELATIVE_WAY).resolve(NOT_VALID_FILES).toAbsolutePath();
     }
 
     @Test
@@ -53,9 +51,101 @@ public class ExelParsingFileTest {
         Assertions.assertEquals(expected, actual);
     }
 
+    @Test
+    public void correctCellTypes_Ok() {
+        InputStream inputStream = getCorrectFile("correct_cell_type.xlsx");
+        CallersBase actual = fileParseService.parseExelToCallersBase(inputStream);
+
+        CallersBase expected = new CallersBaseBuilder(Arrays.asList("номер телефона", "test", "test_1", "one two three", "date"))
+                .addPhoneColumnName("номер телефона")
+                .addCallerRow(Arrays.asList("123.0", "dcewa", "ces", "123.0", "44510.0"), true)
+                .addCallerRow(Arrays.asList("8(12)", "cesa", "dcvds", "dcs", "cvds"), true)
+                .build();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void smallTable_Ok() {
+        InputStream inputStream = getCorrectFile("small_table.xlsx");
+        CallersBase actual = fileParseService.parseExelToCallersBase(inputStream);
+
+        CallersBase expected = new CallersBaseBuilder(Arrays.asList("phone number"))
+                .addPhoneColumnName("phone number")
+                .addCallerRow(Arrays.asList("1234.0"), true)
+                .addCallerRow(Arrays.asList("12(3)4"), true)
+                .build();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
     private InputStream getCorrectFile(String fileName) {
         try {
-            Path path = absolutePathCorrectFiles.resolve(fileName);
+            Path path = correctFiles.resolve(fileName);
+            return Files.newInputStream(path);
+        } catch (IOException e) {
+            throw new FileParsingException(e.getMessage());
+        }
+    }
+
+    @Test
+    public void emptyFile_Wrong() {
+        executeWrongTest("empty_file.xlsx");
+    }
+
+    @Test
+    public void cellWithOneSpaceInHeader_Wrong() {
+        executeWrongTest("cell_with_only_space_in_header.xlsx");
+    }
+
+    @Test
+    public void dataHaveRowWithMoreColumnsInMiddle_Wrong() {
+        executeWrongTest("data_fave_more_columns_middle_row.xlsx");
+    }
+
+    @Test
+    public void dataHaveRowWithMoreColumnsInFirstRow_Wrong() {
+        executeWrongTest("data_have_more_columns_first_row.xlsx");
+    }
+
+    @Test
+    public void emptyHeadersCellInMiddle_Wrong() {
+        executeWrongTest("empty_cell_in_middle_in_headers.xlsx");
+    }
+
+    @Test
+    public void emptyFirstHeadersCell_Wrong() {
+        executeWrongTest("empty_first_cell_in_headers.xlsx");
+    }
+
+    @Test
+    public void notCorrectTypeCellsInData_Wrong() {
+        executeWrongTest("not_correct_type_cells_in_data.xlsx");
+    }
+
+    @Test
+    public void onlyOneRowInFile_Wrong() {
+        executeWrongTest("one_row.xlsx");
+    }
+
+    @Test
+    public void twoEqualsNameColumn_Wrong() {
+        executeWrongTest("two_equals_name_column.xlsx");
+    }
+
+    @Test
+    public void twoEqualsNamePhoneColumn_Wrong() {
+        executeWrongTest("two_equals_name_phone_column.xlsx");
+    }
+
+    private void executeWrongTest(String fileName) {
+        InputStream inputStream = getWrongFile(fileName);
+        Assertions.assertThrows(FileParsingException.class, () -> fileParseService.parseExelToCallersBase(inputStream));
+    }
+
+    private InputStream getWrongFile(String fileName) {
+        try {
+            Path path = wrongFiles.resolve(fileName);
             return Files.newInputStream(path);
         } catch (IOException e) {
             throw new FileParsingException(e.getMessage());
