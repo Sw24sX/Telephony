3cd12f81516e4b911f0f72e232bf1be14b0ece99
sw24sx <aleksandr.korolyov.99@mail.ru>
2021-11-04 23:27:19 +0500

    espeak save text to file

--- src/main/java/com/example/telephony/service/TTSService.java
+++ src/main/java/com/example/telephony/service/TTSService.java
@@ -1,16 +1,10 @@
 package com.example.telephony.service;
 
-import ai.rev.speechtotext.ApiClient;
-import ai.rev.speechtotext.models.asynchronous.RevAiAccount;
-import ai.rev.speechtotext.models.asynchronous.RevAiJob;
-import com.example.telephony.exception.TelephonyException;
-import com.google.cloud.texttospeech.v1beta1.TextToSpeechClient;
-import com.harium.hci.espeak.Espeak;
-import com.sun.speech.freetts.Voice;
-import com.sun.speech.freetts.VoiceManager;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -18,6 +12,7 @@ import java.nio.file.Paths;
 public class TTSService {
     private static final String RU = "ru-RU";
     private static final String EN = "en-US";
+    private static final String COMMAND_ESPEAK = "espeak";
 
     public void tts(String text) {
 //        try (TextToSpeechClient textToSpeechClient = TextToSpeechClient.create()){
@@ -30,34 +25,41 @@ public class TTSService {
         espeak(text);
     }
 
-    private void revai(String text) {
-        String token = "02axXUEqB6PxnHa_0pz_jEpViTgPLFihsYcg_zveOcaLJirjQfONab7V-b9N2SS7AIxZgRtPlxXXj3CfiN99wo8-Y06wI";
-        ApiClient apiClient = new ApiClient(token);
-        try {
-            RevAiAccount revAiAccount = apiClient.getAccount();
-            Path path = Paths.get("file\\generate").toAbsolutePath();
-            String fileName = "test.wav";
-            path = path.resolve(fileName);
-            RevAiJob revAiJob = apiClient.submitJobLocalFile(path.toString());
-        } catch (IOException e) {
-            throw new TelephonyException(e.getMessage());
-        }
-
-    }
-
     private void espeak(String text) {
-        Espeak espeak = new Espeak();
-        espeak.speak(text);
-
+//        Espeak espeak = new Espeak();
+//        espeak.speak(text);
+        Path path = Paths.get("file\\generate");
+        execute(COMMAND_ESPEAK, "-w", path.toAbsolutePath().resolve("text.wav").toString(), text);
     }
 
-    private void freeTTS(String text) {
-        System.setProperty("freetts.voices", "com.sun.speech.freetts.en.us.cmu_us_kal.KevinVoiceDirectory");
-        VoiceManager voiceManager = VoiceManager.getInstance();
-        Voice[] voices = voiceManager.getVoices();
-        Voice voice = voiceManager.getVoice("kevin16");
-        voice.allocate();
-        voice.speak(text);
-        voice.deallocate();
+    private static void execute(final String ... command) {
+        String threadName = "espeak";
+
+        new Thread(new Runnable() {
+            public void run() {
+                ProcessBuilder b = new ProcessBuilder(command);
+                b.redirectErrorStream(true);
+                try {
+                    Process process = b.start();
+
+                    readErrors(process);
+                    process.waitFor();
+                    process.destroy();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            private void readErrors(Process process) throws IOException {
+                BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                String line;
+                while ((line = in.readLine()) != null) {
+                    System.err.println(line);
+                }
+            }
+        }, threadName).start();
     }
 }
