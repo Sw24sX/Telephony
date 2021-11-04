4669133e944e06c46f67257b5a5188375d8ae2c9
sw24sx <aleksandr.korolyov.99@mail.ru>
2021-11-04 14:14:21 +0500

    change references between caller and callers_base

--- src/main/resources/db/migration/V7__Add_Vaiables_In_Caller_Base.sql
+++ src/main/resources/db/migration/V7__Add_Vaiables_In_Caller_Base.sql
@@ -5,3 +5,7 @@ alter table "caller_base" add "variables_list" varchar(125) ARRAY NOT NULL;
 alter table "caller" drop "name";
 
 alter table "caller" add "valid" boolean NOT NULL default FALSE;
+
+alter table "caller_base_callers" drop constraint "caller_base_callers_caller_base_id_caller_id_key";
+
+alter table "caller_base_callers" add constraint "unique_caller_id" unique ("caller_id");
