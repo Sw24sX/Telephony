7e3a2b8024af569a826c7f7b62197c2410de55bf
IgorButymov <60879209+IgorButymov@users.noreply.github.com>
2021-12-07 10:34:50 +0500

    Update FileStorageConfiguration.java

--- src/main/java/com/example/telephony/configuration/FileStorageConfiguration.java
+++ src/main/java/com/example/telephony/configuration/FileStorageConfiguration.java
@@ -4,6 +4,8 @@ import com.example.telephony.service.file.FileStorageService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 
+// TODO: почему аннотация @Service
+// TODO: все другие классы в пакете называются "*Config", этот почему-то "*Configuration"
 @Service
 public class FileStorageConfiguration implements CommandLineRunner {
     private final FileStorageService fileStorageService;
