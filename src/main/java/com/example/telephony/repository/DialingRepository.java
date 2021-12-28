5e7a7b2594bff743979804a19e56e642529e8fbd
sw24sx <aleksandr.korolyov.99@mail.ru>
2021-12-28 20:59:01 +0500

    add filter by status and fix by name

--- src/main/java/com/example/telephony/repository/DialingRepository.java
+++ src/main/java/com/example/telephony/repository/DialingRepository.java
@@ -1,10 +1,13 @@
 package com.example.telephony.repository;
 
 import com.example.telephony.domain.Dialing;
+import com.example.telephony.enums.DialingStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface DialingRepository extends JpaRepository<Dialing, Long> {
-    Page<Dialing> findAll(Pageable pageable);
+    @Query("select d from Dialing d where d.name like ?1 and d.status = ?2")
+    Page<Dialing> findAll(String name, DialingStatus status, Pageable pageable);
 }
