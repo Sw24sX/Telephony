ce9a6144fc5e6f6a8c882de0b25909b979667442
sw24sx <aleksandr.korolyov.99@mail.ru>
2022-01-04 19:26:18 +0500

    save dialing statistic

--- src/main/java/com/example/telephony/configuration/DialingManagerConfig.java
+++ src/main/java/com/example/telephony/configuration/DialingManagerConfig.java
@@ -1,6 +1,7 @@
 package com.example.telephony.configuration;
 
 import com.example.telephony.repository.DialingRepository;
+import com.example.telephony.repository.DialingStatisticRepository;
 import com.example.telephony.service.scenario.dialing.DialingManager;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
@@ -10,14 +11,16 @@ import org.springframework.context.annotation.Scope;
 @Configuration
 public class DialingManagerConfig {
     private final DialingRepository dialingRepository;
+    private final DialingStatisticRepository dialingStatisticRepository;
 
-    public DialingManagerConfig(DialingRepository dialingRepository) {
+    public DialingManagerConfig(DialingRepository dialingRepository, DialingStatisticRepository dialingStatisticRepository) {
         this.dialingRepository = dialingRepository;
+        this.dialingStatisticRepository = dialingStatisticRepository;
     }
 
     @Bean
     @Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
     public DialingManager dialingManager() {
-        return new DialingManager(dialingRepository);
+        return new DialingManager(dialingRepository, dialingStatisticRepository);
     }
 }
