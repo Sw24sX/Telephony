7f3a91df32679ff49d58b63209e363106b7a5090
sw24sx <aleksandr.korolyov.99@mail.ru>
2022-01-05 15:00:57 +0500

    fix success chart

--- src/main/java/com/example/telephony/mapper/dialing/DialingSuccessCallsMapper.java
+++ src/main/java/com/example/telephony/mapper/dialing/DialingSuccessCallsMapper.java
@@ -4,6 +4,7 @@ import com.example.telephony.domain.dialing.Dialing;
 import com.example.telephony.domain.dialing.DialingCallerResult;
 import com.example.telephony.dto.dialing.charts.succes.calls.DialingResultSuccessCallsChartDto;
 import com.example.telephony.enums.DialCallerStatus;
+import com.example.telephony.exception.TelephonyException;
 import com.example.telephony.service.DialingService;
 import lombok.Data;
 import org.apache.commons.collections4.CollectionUtils;
@@ -25,31 +26,43 @@ public abstract class DialingSuccessCallsMapper {
             return new ArrayList<>();
         }
 
-        countSteps = Math.min(countSteps, results.size());
-        Date startDate = results.get(0).getCreated();
-        Date endDate = results.get(results.size() - 1).getCreated();
-        long step = (endDate.getTime() - startDate.getTime()) / countSteps;
-        return calculateChart(results, step, new Date(startDate.getTime() + step));
+        long startDate = results.get(0).getCreated().getTime();
+        long endDate = results.get(results.size() - 1).getCreated().getTime();
+        long step = (long) Math.ceil((endDate - startDate) / (double) countSteps);
+        return calculateChart(results, createSteps(step, startDate, countSteps));
     }
 
-    private List<DialingResultSuccessCallsChartDto> calculateChart(List<DialingCallerResult> callerResults, long step, Date firstStep) {
-        Date currentStepDate = new Date(firstStep.getTime());
-
+    private List<DialingResultSuccessCallsChartDto> createSteps(long step, long startDate, int countStep) {
         List<DialingResultSuccessCallsChartDto> result = new ArrayList<>();
-        result.add(new DialingResultSuccessCallsChartDto(0, currentStepDate));
+        for (int i = 1; i <= countStep; i++) {
+            result.add(new DialingResultSuccessCallsChartDto(0, new Date(startDate + step * i)));
+        }
+        return result;
+    }
 
-        for (DialingCallerResult dialingResult : callerResults) {
-            long currentTime = currentStepDate.getTime();
-            long resultTime = dialingResult.getCreated().getTime();
-            if (dialingResult.getCreated().getTime() > currentStepDate.getTime()) {
-                currentStepDate = new Date(currentStepDate.getTime() + step);
-                result.add(new DialingResultSuccessCallsChartDto(0, currentStepDate));
-            }
-
-            DialingResultSuccessCallsChartDto point = result.get(result.size() - 1);
-            point.setSuccessCalls(point.getSuccessCalls() + 1);
+    private List<DialingResultSuccessCallsChartDto> calculateChart(List<DialingCallerResult> callerResults, List<DialingResultSuccessCallsChartDto> steps) {
+        if (CollectionUtils.isEmpty(steps) || CollectionUtils.isEmpty(callerResults)) {
+            return steps;
         }
 
-        return result;
+        int currentStepDateIndex = 0;
+        int currentResultIndex = 0;
+        while(currentResultIndex < callerResults.size()) {
+            DialingResultSuccessCallsChartDto currentStep = steps.get(currentStepDateIndex);
+            if (callerResults.get(currentResultIndex).getCreated().getTime() > currentStep.getDate().getTime()) {
+                if (currentStepDateIndex + 1 >= steps.size()) {
+                    //todo correct message
+                    throw new TelephonyException("Incorrect steps for chart");
+                }
+
+                currentStepDateIndex += 1;
+                continue;
+            }
+
+            currentStep.setSuccessCalls(currentStep.getSuccessCalls() + 1);
+            currentResultIndex++;
+        }
+
+        return steps;
     }
 }
