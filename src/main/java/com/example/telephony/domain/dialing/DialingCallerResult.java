c9b10bb4e65d22dfc8fb69dc29db89d675566613
sw24sx <aleksandr.korolyov.99@mail.ru>
2022-01-21 17:57:29 +0500

    add new fields to jpa model from migration

--- src/main/java/com/example/telephony/domain/dialing/DialingCallerResult.java
+++ src/main/java/com/example/telephony/domain/dialing/DialingCallerResult.java
@@ -11,6 +11,7 @@ import org.hibernate.annotations.Type;
 import org.hibernate.annotations.TypeDef;
 
 import javax.persistence.*;
+import java.time.LocalTime;
 import java.util.Date;
 import java.util.List;
 import java.util.Objects;
@@ -42,11 +43,11 @@ public class DialingCallerResult extends BaseEntity {
     @Convert(converter = DialCallerStatusConverter.class)
     private DialCallerStatus status;
 
-    @Column(name = "start_call")
-    private Date startCall;
+    @Column(name = "start_call_from_start_day")
+    private LocalTime startCall;
 
-    @Column(name = "end_date")
-    private Date endCall;
+    @Column(name = "end_call_from_start_day")
+    private LocalTime endCall;
 
     @Override
     public boolean equals(Object o) {
