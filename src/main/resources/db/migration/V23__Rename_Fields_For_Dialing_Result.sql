4226c0b0eb2f7c2e8c31ca2b34696b1218b61d90
sw24sx <aleksandr.korolyov.99@mail.ru>
2022-01-21 17:56:34 +0500

    fix migration

--- src/main/resources/db/migration/V23__Rename_Fields_For_Dialing_Result.sql
+++ src/main/resources/db/migration/V23__Rename_Fields_For_Dialing_Result.sql
@@ -1,6 +1,5 @@
 alter table "dialing_caller_result"
-    add column "start_call_from_start_day" int4 not null default 0,
-    add column "end_call_from_start_day" int4 not null default 0,
-    add constraint "start_and_end_date" check ( "start_call_from_start_day" <= 86400000 and "end_call_from_start_day" <= 86400000 ),
+    add column "start_call_from_start_day" time not null default now(),
+    add column "end_call_from_start_day" time not null default now(),
     drop column "start_call",
     drop column "end_date";
