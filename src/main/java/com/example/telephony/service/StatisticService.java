9e5f1d8ffc7fa592c139742ca95f5e2af5406a7a
sw24sx <aleksandr.korolyov.99@mail.ru>
2022-01-22 13:27:54 +0500

    create class for build pie chart

--- src/main/java/com/example/telephony/service/StatisticService.java
+++ src/main/java/com/example/telephony/service/StatisticService.java
@@ -1,7 +1,8 @@
 package com.example.telephony.service;
 
 import com.example.telephony.common.CustomTime;
-import com.example.telephony.service.charts.PointingChart.PointingChart;
+import com.example.telephony.service.charts.pie.PieChart;
+import com.example.telephony.service.charts.pointing.PointingChart;
 import com.example.telephony.domain.dialing.Dialing;
 import com.example.telephony.dto.CommonStatisticDto;
 import com.example.telephony.dto.dialing.charts.pie.DialingResultPartPieChartDto;
@@ -12,6 +13,7 @@ import com.example.telephony.repository.DialingCallerResultRepository;
 import com.example.telephony.repository.DialingStatisticRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalTime;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -30,79 +32,23 @@ public class StatisticService {
 
     public List<DialingResultSuccessCallsChartDto> createSuccessChartByDialing(Long dialingId) {
         Dialing dialing = dialingService.getById(dialingId);
-        return new PointingChart(dialingService.getSuccessCallersResultOrderByCreatedDateByDialing(dialing))
+        return new PointingChart(dialingService.getAllStartCallTimeByDialing(dialing))
                 .createChart()
                 .collect(Collectors.toList());
     }
 
     public List<DialingResultSuccessCallsChartDto> createSuccessChartByAll() {
-        return new PointingChart(dialingService.getSuccessCallersResultOrderByCreatedDate())
+        return new PointingChart(dialingService.getAllStartCallTime())
                 .createChart()
                 .collect(Collectors.toList());
     }
 
     public DialingResultPieChartDto createPieChartAllStatistic() {
-        int count = dialingService.getAllCountDialingsCallers();
-        int countInProgress = count;
-        int countSuccess = 0;
-        List<DialingResultPartPieChartDto> parts = new ArrayList<>();
-        for (DialCallerStatus status : DialCallerStatus.values()) {
-            if (status == DialCallerStatus.IN_PROGRESS) {
-                continue;
-            }
-
-            int percentCount = dialingService.getAllCountDialsByStatus(status);
-            countInProgress -= percentCount;
-
-            int percent = calculatePercent(percentCount, count);
-            parts.add(new DialingResultPartPieChartDto(status.getMessage(), percent, status.name()));
-
-            if (status == DialCallerStatus.CORRECT) {
-                countSuccess = percentCount;
-            }
-        }
-        parts.add(new DialingResultPartPieChartDto(DialCallerStatus.IN_PROGRESS.getMessage(), calculatePercent(countInProgress, count), DialCallerStatus.IN_PROGRESS.name()));
-
-        DialingResultPieChartDto result = new DialingResultPieChartDto();
-        result.setParts(parts);
-        result.setCountCallers(count);
-        result.setCountSuccess(countSuccess);
-        result.setPercentSuccess(calculatePercent(result.getCountSuccess(), result.getCountCallers()));
-        return result;
+        return new PieChart(dialingService).createChart();
     }
 
     public DialingResultPieChartDto createPieChartByDialing(Dialing dialing) {
-        int count = dialingService.getCountDialingCallers(dialing);
-        int countInProgress = count;
-        int countSuccess = 0;
-        List<DialingResultPartPieChartDto> parts = new ArrayList<>();
-        for (DialCallerStatus status : DialCallerStatus.values()) {
-            if (status == DialCallerStatus.IN_PROGRESS) {
-                continue;
-            }
-
-            int percentCount = dialingService.getCountDialsByStatus(dialing, status);
-            countInProgress -= percentCount;
-
-            int percent = calculatePercent(percentCount, count);
-            parts.add(new DialingResultPartPieChartDto(status.getMessage(), percent, status.name()));
-
-            if (status == DialCallerStatus.CORRECT) {
-                countSuccess = percentCount;
-            }
-        }
-        parts.add(new DialingResultPartPieChartDto(DialCallerStatus.IN_PROGRESS.getMessage(), calculatePercent(countInProgress, count), DialCallerStatus.IN_PROGRESS.name()));
-
-        DialingResultPieChartDto result = new DialingResultPieChartDto();
-        result.setParts(parts);
-        result.setCountCallers(count);
-        result.setCountSuccess(countSuccess);
-        result.setPercentSuccess(calculatePercent(result.getCountSuccess(), result.getCountCallers()));
-        return result;
-    }
-
-    private int calculatePercent(int percentCount, int count) {
-        return (percentCount * 100) / count;
+        return new PieChart(dialingService).createChart(dialing);
     }
 
     public CommonStatisticDto createCommonStatistic() {
@@ -111,7 +57,8 @@ public class StatisticService {
         result.setTotalDialings(dialingStatisticRepository.getCountDialingsRun());
         result.setAverageNumberOfCallsPerDialing(dialingCallerResultRepository.getAverageCountCallers());
         Double suc = dialingCallerResultRepository.getAverageCallDuration();
-        result.setAverageCallDuration(CustomTime.fromMilliseconds(suc));
+
+        result.setAverageCallDuration(CustomTime.fromLocalTime(LocalTime.ofSecondOfDay(suc.intValue())));
         return  result;
     }
 }
