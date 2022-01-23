906b4128ce3501ee042df701cdcaab24a96c087e
sw24sx <aleksandr.korolyov.99@mail.ru>
2022-01-23 22:12:45 +0500

    init second microservice for text to speech synthesis

---
+++ TextToSpeechService/src/test/java/com/example/text/to/speech/service/TextToSpeechServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.text.to.speech.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextToSpeechServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
