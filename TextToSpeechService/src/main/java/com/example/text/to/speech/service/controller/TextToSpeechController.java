ca0abcf0d23a73d49919354a67cdab6db4437ce9
sw24sx <aleksandr.korolyov.99@mail.ru>
2022-01-27 11:04:48 +0500

    change sox and add many tts endpoint

--- TextToSpeechService/src/main/java/com/example/text/to/speech/service/controller/TextToSpeechController.java
+++ TextToSpeechService/src/main/java/com/example/text/to/speech/service/controller/TextToSpeechController.java
@@ -3,12 +3,16 @@ package com.example.text.to.speech.service.controller;
 import com.example.text.to.speech.service.dto.CreatedAudioFileResponse;
 import com.example.text.to.speech.service.dto.SpeechTextRequest;
 import com.example.text.to.speech.service.service.TextToSpeechService;
+import ie.corballis.sox.WrongParametersException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController()
 @RequestMapping("tts")
@@ -20,7 +24,12 @@ public class TextToSpeechController {
     }
 
     @PostMapping("create")
-    public CreatedAudioFileResponse textToSpeech(@RequestBody SpeechTextRequest speechTextRequest) throws IOException {
+    public CreatedAudioFileResponse textToSpeech(@RequestBody SpeechTextRequest speechTextRequest) throws IOException, WrongParametersException {
         return textToSpeechService.textToSpeech(speechTextRequest);
+    }
+
+    @PostMapping("create/list")
+    public List<CreatedAudioFileResponse> textToSpeechList(@RequestBody List<SpeechTextRequest> request) {
+        return textToSpeechService.textToSpeechList(request);
     }
 }
--- TextToSpeechService/src/main/java/com/example/text/to/speech/service/service/TextToSpeechService.java
+++ TextToSpeechService/src/main/java/com/example/text/to/speech/service/service/TextToSpeechService.java
@@ -2,45 +2,75 @@ package com.example.text.to.speech.service.service;
 
 import com.example.text.to.speech.service.dto.CreatedAudioFileResponse;
 import com.example.text.to.speech.service.dto.SpeechTextRequest;
+import com.example.text.to.speech.service.exception.TextToSpeechException;
 import com.example.text.to.speech.service.service.command.line.CommandLineExecutor;
 import com.example.text.to.speech.service.service.command.line.SoxCommands;
 import com.google.cloud.texttospeech.v1.*;
+import com.google.common.io.Files;
 import com.google.protobuf.ByteString;
+import ie.corballis.sox.Sox;
+import ie.corballis.sox.WrongParametersException;
+import org.apache.commons.io.FileUtils;
 import org.springframework.stereotype.Service;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 @Service
 public class TextToSpeechService {
-    public CreatedAudioFileResponse textToSpeech(SpeechTextRequest request) throws IOException {
+    public List<CreatedAudioFileResponse> textToSpeechList(List<SpeechTextRequest> requests) {
+        return requests.stream().map(this::textToSpeech).collect(Collectors.toList());
+    }
+
+    public CreatedAudioFileResponse textToSpeech(SpeechTextRequest request) {
         CreatedAudioFileResponse result = new CreatedAudioFileResponse(request);
 
         GoogleCloudTTS tts = new GoogleCloudTTS();
         ByteString audioContents = tts.textToSpeech(request);
-        String fileName = writeOutputFile(audioContents);
+        String fileName =  writeOutputFile(audioContents);
+        result.setName(fileName);
+        result.setUri(Paths.get(fileName).toAbsolutePath().toString());
 
-        SoxCommands sox = new SoxCommands();
-        sox.addCommand(fileName, generateNewFileName("wav"));
-
-        CommandLineExecutor executor = new CommandLineExecutor();
-        executor.addCommands(sox);
-        executor.execute();
+        reformatFile(fileName);
 
         return result;
     }
 
-    private String writeOutputFile(ByteString audioContents) throws IOException {
-        String fileName = generateNewFileName("mp3");
-        try (OutputStream out = new FileOutputStream(fileName)) {
-            out.write(audioContents.toByteArray());
-            // TODO: 23.01.2022 log this
-            System.out.printf("Audio content written to file \"%s\"", fileName);
-        }
+    private String writeOutputFile(ByteString audioContents) {
+        String fileName = generateNewFileName("raw");
+        File resultFile = Paths.get(fileName).toFile();
 
+        try {
+            FileUtils.writeByteArrayToFile(resultFile, audioContents.toByteArray());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return fileName;
+    }
+
+    private String reformatFile(String fileName) {
+        String soxPath = Paths.get("sox-14-4-2\\sox.exe").toAbsolutePath().toString();
+        Sox sox = new Sox(soxPath);
+        String resultFileName = generateNewFileName("wav");
+        try {
+            sox
+                .bits(16)
+                .argument("-e", "signed-integer")
+                .argument("-c", "1")
+                .inputFile(fileName)
+                .outputFile(resultFileName)
+                .execute();
+        } catch (IOException | WrongParametersException e ) {
+            throw new TextToSpeechException(e.getMessage(), e.getCause());
+        }
+        return resultFileName;
     }
 
     private String generateNewFileName(String format) {
