e712543ad73022acee5fb784221bbce33de2e025
sw24sx <aleksandr.korolyov.99@mail.ru>
2022-01-28 20:28:26 +0500

    init service DialerService

---
+++ DialerService/src/main/java/com/example/dialerservice/DialerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.dialerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DialerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DialerServiceApplication.class, args);
+    }
+
+}
---
+++ DialerService/src/test/java/com/example/dialerservice/DialerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dialerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DialerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
